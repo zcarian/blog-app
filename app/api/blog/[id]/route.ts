12c93692546e9fe7619d536ced9b1cb7b12a65d3
zcarian <mar.jed.00@gmail.com>
2023-07-04 14:17:35 +0200

    creating dynaminc delete handler

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -34,4 +34,15 @@ export const PUT = async (req: Request, res: NextResponse) => {
   }
 };
 
-export const DELETE = async (req: Request, res: NextResponse) => {};
+export const DELETE = async (req: Request, res: NextResponse) => {
+  try {
+    const id = req.url.split("/blog/")[1];
+    await main();
+    const post = await prisma.post.delete({ where: { id } });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
