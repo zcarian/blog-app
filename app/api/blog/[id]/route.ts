19d77a0130851f3aeacd13ac4f22c78adc9f7368
zcarian <mar.jed.00@gmail.com>
2023-07-04 14:05:48 +0200

    creating dynaminc put handler

--- app/api/blog/[id]/route.ts
+++ app/api/blog/[id]/route.ts
@@ -17,6 +17,21 @@ export const GET = async (req: Request, res: NextResponse) => {
   }
 };
 
-export const PUT = async (req: Request, res: NextResponse) => {};
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const id = req.url.split("/blog/")[1];
+    const { title, description } = await req.json();
+    await main();
+    const post = await prisma.post.update({
+      data: { title, description },
+      where: { id },
+    });
+    return NextResponse.json({ message: "Success", post }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
 
 export const DELETE = async (req: Request, res: NextResponse) => {};
