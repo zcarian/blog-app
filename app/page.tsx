63c5f10f3fceca605bd03ca852e9430049a32a34
zcarian <mar.jed.00@gmail.com>
2023-07-04 15:17:30 +0200

    fetching data and adding style to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,34 @@
-export default function Home() {
+import Link from "next/link";
+
+async function fetchBlogs() {
+  const res = await fetch("http://localhost:3000/api/blog", {
+    next: {
+      revalidate: 10,
+    },
+  });
+  const data = await res.json();
+  return data.posts;
+}
+
+export default async function Home() {
+  // const posts = await fetchBlogs();
+  // console.log(posts);
+
   return (
-    <main>Hi</main>
-  )
+    <main className="w-full h-full">
+      <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-4 rounded-lg bg-slate-800 drop-shadow-xl">
+        <h1 className="text-slate-200 text-center text-2xl font-extrabold font-[verdana]">
+          Fluu Stack Blog app
+        </h1>
+      </div>
+      <div className="flex">
+        <Link
+          href="/blog/add"
+          className="md:w-1/6 sm:w-2/4 text-center rounded-md p-2 m-auto bg-slate-200 font-semibold "
+        >
+          Add New Blog 📝
+        </Link>
+      </div>
+    </main>
+  );
 }
