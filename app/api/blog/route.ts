7ac96f00717f58dca11668685eb40f87bc09fe85
zcarian <mar.jed.00@gmail.com>
2023-07-04 13:45:56 +0200

    adding post handler

--- app/api/blog/route.ts
+++ app/api/blog/route.ts
@@ -18,9 +18,20 @@ export const GET = async (req: Request, res: NextResponse) => {
     return NextResponse.json({ message: "Success", posts }, { status: 200 });
   } catch (err) {
     return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
   }
 };
 
 export const POST = async (req: Request, res: NextResponse) => {
-  console.log("POST");
+  try {
+    const { title, description } = await req.json();
+    await main();
+    const post = await prisma.post.create({ data: { description, title } });
+    return NextResponse.json({ message: "Success", post }, { status: 201 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
 };
